84dd01cb820f1b9f34bd239c31eb454095ab3a25
hkhch <macbook@Macbooks-MacBook-Pro.local>
2019-08-27 11:27:41 +0900

    개발 기본 환경구축

---
+++ my_settings.py
@@ -0,0 +1,17 @@
+# from datatime import datetime, timedelta
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django', 
+        'NAME': 'WOORIDOORI',
+        'USER': 'root',
+        'PASSWORD': 'hkhch1335',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
+
+SECRET = {
+     'secret': 'mqrc53=+!22m9#x5xbr!f8e*$9qtq-19@+g-j+6@rckp_oomdk'
+}
+
