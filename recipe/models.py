21924faf69ceb3d39bb702a8991933295245125e
hkhch <macbook@Macbooks-MacBook-Pro.local>
2019-09-07 20:02:19 +0900

    Review 사항 2차 수정

--- recipe/models.py
+++ recipe/models.py
@@ -2,35 +2,26 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
-
 class Category(models.Model):
-    category_no = models.IntegerField()
+    category_number = models.IntegerField()
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.category_no
-
-
 class CategoryItem(models.Model):
-    item_no = models.IntegerField()
+    item_number = models.IntegerField()
     name = models.CharField(max_length=50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.item_no
-
-
 class RecipeItem(models.Model):
-    recipe_no = models.IntegerField()
+    recipe_number = models.IntegerField()
     title = models.CharField(max_length=500)
     name = models.CharField(max_length=500)
     method = models.CharField(max_length=2000)
     image = models.URLField(max_length=300, null=True)
-    categoryItem = models.ForeignKey(CategoryItem, on_delete=models.CASCADE)
+    category_item = models.ForeignKey(CategoryItem, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- recipe/views.py
+++ recipe/views.py
@@ -3,7 +3,6 @@ from django.http import JsonResponse, HttpResponse
 from django.views import View
 from .models import Category, CategoryItem, RecipeItem
 import json
-import ipdb
 
 RETURN_MESSAGE = {
     "DOES_NOT_EXIST": "데이터가 존재하지 않습니다." 
@@ -13,9 +12,9 @@ class CategoryView(View):
   
     def get(self, request):
         try:
-            categoryList = list(CategoryItem.objects.order_by('item_no').
+            categoryList = list(CategoryItem.objects.order_by('item_number').
                             select_related('category').values(
-                                'item_no',
+                                'item_number',
                                 'name',
                                 'category__name'
                             ))
@@ -26,25 +25,25 @@ class CategoryView(View):
 class RecipeView(View):
 
     def get(self, request):
-        categoryItem_number = int(request.GET.get("categoryItem_number", "0"))
+        category_item_number = int(request.GET.get("category_item_number", "0"))
         start_offset = int(request.GET.get("start_offset", "0"))
         recipe_count = int(request.GET.get("recipe_count", "1"))
 
         try:
-            max_recipe = RecipeItem.objects.filter(categoryItem__item_no=categoryItem_number).count()
+            max_recipe = RecipeItem.objects.filter(category_item__item_number=category_item_number).count()
             ideal_start_index = start_offset
             start_index = max_recipe-1 if ideal_start_index > max_recipe else ideal_start_index
             ideal_last_index = start_offset + recipe_count
             last_index = max_recipe if ideal_last_index > max_recipe  else ideal_last_index
 
-            result = list(RecipeItem.objects.filter(categoryItem__item_no=categoryItem_number).values(
-                            'recipe_no',
+            result = list(RecipeItem.objects.filter(category_item__item_number=category_item_number).values(
+                            'recipe_number',
                             'title',
                             'name',
                             'method',
                             'image',
-                            'categoryItem__item_no',
-                            'categoryItem__name'))[start_index:last_index]
+                            'category_item__item_number',
+                            'category_item__name'))[start_index:last_index]
             return JsonResponse({
                             "reciep_total" : max_recipe,
                             "result": result}, status=200) 
